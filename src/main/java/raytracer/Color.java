63197ae943e74038581f52fd99d8b6e44ec91ba4
Max Otto <max.otto@fottografie.net>
2026-04-03 17:18:10 +0200

    Added Color class + tests (Aufgabe 4)

---
+++ src/main/java/raytracer/Color.java
@@ -0,0 +1,27 @@
+package raytracer;
+
+public class Color {
+    double r, g, b;
+
+    public Color(double r, double g, double b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public Color plus(Color other){
+        return new Color(this.r + other.r, this.g + other.g, this.b + other.b);
+    }
+
+    public Color scaled(double s){
+        return new Color(this.r * s, this.g * s, this.b * s);
+    }
+
+    public java.awt.Color toAwtColor(){
+        return new java.awt.Color((float)Math.min(this.r, 1), (float)Math.min(this.g, 1), (float)Math.min(this.b, 1));
+    }
+
+    public String toString(){
+        return "(" + this.r + ", " + this.g + ", " + this.b + ")";
+    }
+}
---
+++ src/test/java/raytracer/ColorTest.java
@@ -0,0 +1,43 @@
+package raytracer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ColorTest {
+
+    @Test
+    void testConstructor() {
+        Color red = new Color(1, 0, 0);
+
+        assertEquals("(1.0, 0.0, 0.0)", red.toString());
+    }
+
+    @Test
+    void testYellow() {
+        Color red = new Color(1, 0, 0);
+        Color green = new Color(0, 1, 0);
+
+        assertEquals("(1.0, 1.0, 0.0)", red.plus(green).toString());
+    }
+
+    @Test
+    void testYellowScaled() {
+        Color red = new Color(1, 0, 0);
+        Color green = new Color(0, 1, 0);
+
+        Color yellow = red.plus(green);
+
+        assertEquals("(0.5, 0.5, 0.0)", yellow.scaled(0.5).toString());
+    }
+
+    @Test
+    void testYellowToAwt() {
+        Color red = new Color(1, 0, 0);
+        Color green = new Color(0, 1, 0);
+
+        Color yellow = red.plus(green);
+
+        assertEquals("java.awt.Color[r=255,g=255,b=0]", yellow.toAwtColor().toString());
+    }
+}
