20f3e71a7e0088f4ea87351a16c35d45b46be899
Max Otto <max.otto@fottografie.net>
2026-04-04 11:38:40 +0200

    Fixed SphereTest to work with material

--- src/test/java/raytracer/SphereTest.java
+++ src/test/java/raytracer/SphereTest.java
@@ -7,14 +7,16 @@ class SphereTest {
 
     @Test
     void testConstructor() {
-        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1);
+        Material material = new Material(new Color(1,1,1));
+        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1, material);
 
         assertEquals("(0.0, 0.0, 1.0) 1.0", sphere.toString());
     }
 
     @Test
     void testIntersection() {
-        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1);
+        Material material = new Material(new Color(1,1,1));
+        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1, material);
         Ray ray = new Ray(new Vector3d(0,0,-1), new Vector3d(0,0,1));
 
         assertEquals(1.0, sphere.intersection(ray));
@@ -23,7 +25,8 @@ class SphereTest {
 
     @Test
     void testIntersection2() {
-        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1);
+        Material material = new Material(new Color(1,1,1));
+        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1, material);
         Ray ray2 = new Ray(new Vector3d(0,0,-1), new Vector3d(0,1,1).normalized());
 
         assertEquals(-1.0, sphere.intersection(ray2));
@@ -31,7 +34,8 @@ class SphereTest {
 
     @Test
     void testIntersection3() {
-        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1);
+        Material material = new Material(new Color(1,1,1));
+        Sphere sphere = new Sphere(new Vector3d(0,0,1), 1, material);
         Ray ray3 = new Ray(new Vector3d(0,0,-1), new Vector3d(0,0.5,1).normalized());
 
         assertEquals("(0.0, 0.6000000000000001, 0.20000000000000018)", ray3.at(sphere.intersection(ray3)).toString());
