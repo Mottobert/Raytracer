5b69658d039b7cb78e4058d2212a3b9102d80e93
Max Otto <max.otto@fottografie.net>
2026-04-10 17:01:11 +0200

    Added javadoc descriptions to the Sphere class

--- src/main/java/raytracer/Sphere.java
+++ src/main/java/raytracer/Sphere.java
@@ -4,17 +4,33 @@
 
 package raytracer;
 
+/**
+ * Represents a Sphere that can be placed in the 3D scene with the given size and material properties.
+ */
 public class Sphere {
     public final Vector3d center;
     public final double radius;
     public final Material material;
 
+    /**
+     * Creates a new Sphere at the center position, with the given radius and specified material.
+     *
+     * @param center the position of the sphere origin.
+     * @param radius the radius of the sphere.
+     * @param material the material the sphere should be rendered with.
+     */
     public Sphere(Vector3d center, double radius, Material material){
         this.center = center;
         this.radius = radius;
         this.material = material;
     }
 
+    /**
+     * Checks if the given ray intersects with the Sphere and if so returns the closest intersection point from the rays origin.
+     *
+     * @param ray the position of the sphere origin.
+     * @return the closest point of intersection with the ray, returns -1 when no intersection occurs.
+     */
     public double intersection(Ray ray){
         Vector3d originMinusCenter = ray.origin.minus(this.center);
         double b = ray.direction.dot(originMinusCenter);
@@ -41,10 +57,24 @@ public class Sphere {
         }
     }
 
-    public Vector3d normal(Vector3d p){
-        return p.minus(this.center).scaled(1/this.radius);
+    /**
+     * Calculates the normal vector at the given position.
+     *
+     * @param position the position the normal vector should be calculated at.
+     * @return the normal vector at the given position.
+     */
+    public Vector3d normal(Vector3d position){
+        return position.minus(this.center).scaled(1/this.radius);
     }
 
+    /**
+     * Calculates the diffuse color depending on the angle between the normal vector and the light position and the
+     * diffuse color specified in the material.
+     *
+     * @param normal the normal vector.
+     * @param light the light position.
+     * @return the diffuse color.
+     */
     public Color diffuseShading(Vector3d normal, Vector3d light){
         double iDiffuse = Math.max(0, normal.dot(light));
 
@@ -55,6 +85,15 @@ public class Sphere {
         return color.scaled(iDiffuse);
     }
 
+    /**
+     * Calculates the specular color depending on the angle between the normal vector, the light position and the view
+     * direction and the specular shininess and specular intensity specified in the material.
+     *
+     * @param normal the normal vector.
+     * @param light the light position.
+     * @param view the view direction.
+     * @return the diffuse color.
+     */
     public Color specularShading(Vector3d normal, Vector3d light, Vector3d view){
         Vector3d r = light.reflect(normal);
         double shininess = this.material.shininess;
@@ -69,9 +108,17 @@ public class Sphere {
         );
     }
 
-    public Color shading(Vector3d p, Light lightSource, Vector3d view){
-        Vector3d n = normal(p);
-        Vector3d l = lightSource.position.minus(p).normalized();
+    /**
+     * Calculates the diffuse and specular contribution to the shading.
+     *
+     * @param position the position on the sphere.
+     * @param lightSource the light source.
+     * @param view the view direction.
+     * @return the combined color for diffuse and specular lighting.
+     */
+    public Color shading(Vector3d position, Light lightSource, Vector3d view){
+        Vector3d n = normal(position);
+        Vector3d l = lightSource.position.minus(position).normalized();
 
         Color dColor = diffuseShading(n, l);
         Color sColor = specularShading(n, l, view);
@@ -79,11 +126,20 @@ public class Sphere {
         return dColor.plus(sColor);
     }
 
+    /**
+     * Calculates the uv-coordinates for a sphere depending on the normal vector.
+     *
+     * @param normal the normal vector.
+     * @return the normalized uv-coordinates.
+     */
     public double[] makeUVs(Vector3d normal){
         double theta = Math.atan2(normal.z, normal.x);
         double rho = Math.acos(-normal.y);
 
-        return new double[]{theta/(2*Math.PI), rho/Math.PI};
+        return new double[]{
+                theta/(2*Math.PI),
+                rho/Math.PI
+        };
     }
 
     public String toString(){
